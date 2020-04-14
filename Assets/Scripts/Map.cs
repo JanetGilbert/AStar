9f3ed5655d5ddf60488313d7ebb6454b2bf4acf2
Janet Gilbert <janet@wadjeteyegames.com>
2020-04-14 14:13:39 -0400

    Messy implementation of A*

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+//using System;
 
 /* A class to contain the tile grid and the pathfinding functions.*/
 public class Map : MonoBehaviour
@@ -23,6 +23,9 @@ public class Map : MonoBehaviour
     private Vector2Int startPos;
     private Vector2Int destPos;
 
+    // The calculated path;
+    private List<Tile> pathTiles;
+    private int displayIndex; // Current path object to display.
 
     void Start()
     {
@@ -30,14 +33,29 @@ public class Map : MonoBehaviour
         InitGrid();
         PlaceObstacle(2,3,4,2);
 
-        SetStartPos(0, 1);
+        SetStartPos(4, 1);
         SetDestPos(5, 7);
     }
 
-    
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            CalculateDrunkardsWalk();
+            ClearDisplay();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CalculateAStar();
+            ClearDisplay();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AdvanceDisplay();
+        }
     }
 
     // Initialize empty grid.
@@ -56,6 +74,7 @@ public class Map : MonoBehaviour
             {
                 Tile newTile = Instantiate(tilePrefab, tilePos, Quaternion.identity);
                 newTile.transform.parent = transform;
+                newTile.Pos = new Vector2Int(x, y);
                 tileGrid[x, y] = newTile;
 
                 tilePos.y += gridH;
@@ -117,6 +136,171 @@ public class Map : MonoBehaviour
         tileGrid[posX, posY].Type = TileType.End;
     }
 
+    private void CalculateDrunkardsWalk()
+    {
+        Tile start = tileGrid[startPos.x, startPos.y];
+        Tile end = tileGrid[destPos.x, destPos.y];
+        Tile current = start;
+        int sanityCheck = 999; // Make sure we don't wander endlessly if the route is completely blocked.
+
+        pathTiles = new List<Tile>();
+
+        while (!pathTiles.Contains(end) && sanityCheck > 0)
+        {
+            List<Tile> adjacent = new List<Tile>();
+
+            AddTileIfPossible(adjacent, current.Pos + Vector2Int.up);
+            AddTileIfPossible(adjacent, current.Pos + Vector2Int.down);
+            AddTileIfPossible(adjacent, current.Pos + Vector2Int.left);
+            AddTileIfPossible(adjacent, current.Pos + Vector2Int.right);
+
+            if (adjacent.Count > 0)
+            {
+                Tile newCurrent = adjacent[Random.Range(0, adjacent.Count)];
+                pathTiles.Add(newCurrent);
+                current = newCurrent;
+            }
+
+            sanityCheck--;
+        }
+    }
+
+    // Is the position on the board and a valid move position.
+    private bool CanAddTile(Vector2Int pos)
+    {
+        if ((pos.x >= 0) &&
+            (pos.y >= 0) &&
+            (pos.x < sizeX) &&
+            (pos.y < sizeY))
+        {
+            if ((tileGrid[pos.x, pos.y].Type == TileType.Normal) ||
+                (tileGrid[pos.x, pos.y].Type == TileType.End))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void AddTileIfPossible(List<Tile> tiles, Vector2Int pos)
+    {
+        if (CanAddTile(pos))
+        {
+            tiles.Add(tileGrid[pos.x, pos.y]);
+        }
+    }
 
 
+
+
+    private void ClearDisplay()
+    {
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                if (tileGrid[x, y].Type == TileType.Path)
+                {
+                    tileGrid[x, y].Type = TileType.Normal;
+                }
+            }
+        }
+
+        displayIndex = 0;
+    }
+
+    private void AdvanceDisplay()
+    {
+        if (displayIndex < pathTiles.Count)
+        {
+            tileGrid[pathTiles[displayIndex].Pos.x, pathTiles[displayIndex].Pos.y].Type = TileType.Path;
+            displayIndex++;
+        }
+    }
+
+    private void CalculateAStar()
+    {
+        Tile start = tileGrid[startPos.x, startPos.y];
+        Tile end = tileGrid[destPos.x, destPos.y];
+        List<Tile> open = new List<Tile>();
+        List<Tile> closed = new List<Tile>();
+        Tile current = start;
+
+    //   
+
+        open.Add(start);
+
+        start.prev = null;
+
+        while (open.Count > 0 && !open.Contains(end))
+        {
+            int minCost = 9999;
+            int lowestIndex = 0;
+
+            for (int t = 0; t < open.Count; t++)
+            {
+                if (open[t].Cost < minCost)
+                {
+                    minCost = open[t].Cost;
+                    lowestIndex = t;
+                }
+            }
+
+            current = open[lowestIndex];
+            open.Remove(current);
+            closed.Add(current);
+
+
+            List<Tile> adjacent = new List<Tile>();
+
+            AddTileIfPossibleA(adjacent, current.Pos + Vector2Int.up, closed);
+            AddTileIfPossibleA(adjacent, current.Pos + Vector2Int.down, closed);
+            AddTileIfPossibleA(adjacent, current.Pos + Vector2Int.left, closed);
+            AddTileIfPossibleA(adjacent, current.Pos + Vector2Int.right, closed);
+
+            foreach (Tile tile in adjacent)
+            {
+                if (open.Contains(tile))
+                {
+                    if (current.distFromStart + 1 < tile.distFromStart)
+                    {
+                        tile.distFromStart = current.distFromStart + 1;
+                        tile.prev = current;
+                    }
+                }
+                else
+                {
+                    open.Add(tile);
+                    tile.prev = current;
+                    tile.distFromStart = current.distFromStart + 1;
+                    tile.distFromEnd = Mathf.Abs(tile.Pos.x - end.Pos.x) + Mathf.Abs(tile.Pos.y - end.Pos.y);
+                }
+            }
+        }
+
+        // Build path.
+        pathTiles = new List<Tile>();
+        current = end;
+        while (current.prev != null)
+        {
+            pathTiles.Add(current);
+            current = current.prev;
+        }
+
+        pathTiles.Reverse();
+    }
+
+
+
+    private void AddTileIfPossibleA(List<Tile> tiles, Vector2Int pos, List<Tile> closed)
+    {
+        if (CanAddTile(pos))
+        {
+            if (!closed.Contains(tileGrid[pos.x, pos.y]))
+            {
+                tiles.Add(tileGrid[pos.x, pos.y]);
+            }
+        }
+    }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 // States a tile can be in.
-public enum TileType { Normal, Obstacle, Start, End };
+public enum TileType { Normal, Obstacle, Start, End, Path };
 
 /* Display and store data about tiles */
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IEquatable<Tile>
 {
+    [SerializeField]
+    private GameData gameData; // The scriptable object that holds data about tile definitions.
+
+
     private TileType _type;
     public TileType Type
     {
@@ -25,11 +30,25 @@ public class Tile : MonoBehaviour
         }
     }
 
-    private SpriteRenderer spriteRenderer; // Get the renderer of the sprite so we can change its color.
+    private SpriteRenderer spriteRenderer; // Cache the renderer of the sprite so we can change its color.
 
+    private Vector2Int _pos; // Position in grid
 
-    [SerializeField]
-    private GameData gameData; // The scriptable object that holds data about tile definitions.
+    public Vector2Int Pos
+    {
+        get
+        {
+            return _pos;
+        }
+
+        set
+        {
+            _pos = value;
+        }
+    }
+
+   
+
 
     void Awake()
     {
@@ -48,5 +67,75 @@ public class Tile : MonoBehaviour
     }
 
 
+    // A* stuff
+    public Tile prev;
+    public int distFromStart;
+    public int distFromEnd;
+    public int Cost
+    {
+        get
+        {
+            return distFromStart + distFromEnd;
+        }
+    }
 
+    // From https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/how-to-define-value-equality-for-a-type
+    public override bool Equals(object obj)
+    {
+        return this.Equals(obj as Tile);
+    }
+
+    public bool Equals(Tile t)
+    {
+        // If parameter is null, return false.
+        if (System.Object.ReferenceEquals(t, null))
+        {
+            return false;
+        }
+
+        // Optimization for a common success case.
+        if (System.Object.ReferenceEquals(this, t))
+        {
+            return true;
+        }
+
+        // If run-time types are not exactly the same, return false.
+        if (this.GetType() != t.GetType())
+        {
+            return false;
+        }
+
+        // Return true if the fields match.
+        // Note that the base class is not invoked because it is
+        // System.Object, which defines Equals as reference equality.
+        return (t.Pos == Pos);
+    }
+
+    public override int GetHashCode()
+    {
+        return Pos.x * 0x00010000 + Pos.y;
+    }
 }
+
+
+/*public class PathTile
+{
+    public Tile tile;
+    public int distFromStart;
+    public int distFromEnd;
+    public int Value
+    {
+        get
+        {
+            return distFromStart + distFromEnd;
+        }
+    }
+
+    public PathTile(Tile linkedTile, PathTile prev, Tile dest)
+    {
+        tile = linkedTile;
+        distFromStart = prev.distFromStart + 1;
+        distFromEnd = Mathf.Abs(tile.Pos.x - dest.Pos.x) + Mathf.Abs(tile.Pos.y - dest.Pos.y);
+    }
+
+}*/
